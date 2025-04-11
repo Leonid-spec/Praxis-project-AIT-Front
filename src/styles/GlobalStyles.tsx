d72517b807b439b4be0311158cc5fa21deea12fc
YuliiaSydorenko <o972986990f@gmail.com>
2025-04-11 14:13:35 +0200

    @media\GlobalStyles

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -18,7 +18,44 @@ h1, h2, h3, h4, h5, h6, p {
   margin: 0;
   padding: 0;
 }
-`
+
+@media (max-width: 868px) {
+    body {
+      font-size: 14px; /* Общий размер текста для тела */
+    }
+
+    h1 {
+      font-size: 20px; /* Размер шрифта для заголовков */
+    }
+
+    h2 {
+      font-size: 18px;
+    }
+
+    p {
+      font-size: 12px; /* Размер шрифта для параграфов */
+    }
+  }
+
+  @media (max-width: 668px) {
+    body {
+      font-size: 12px; /* Общий размер текста для тела */
+    }
+
+    h1 {
+      font-size: 18px; /* Размер шрифта для заголовков */
+    }
+
+    h2 {
+      font-size: 16px; /* Размер шрифта для подзаголовков */
+    }
+
+    p {
+      font-size: 10px; /* Размер шрифта для параграфов */
+    }
+}
+
+`;
 
 function GlobalStyles() {
   return <Global styles={globalStyles} />
