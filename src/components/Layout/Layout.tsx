9649c095cde632e36f76b63be60ae3a10f24b6c8
Tanya Maxar <kolt.levry@gmail.com>
2025-04-14 07:01:44 +0200

    try to merge

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,7 @@
 import { ReactNode } from "react";
 import { useLocation } from "react-router-dom";
 import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import MiniFooter from "../../pages/Contacts/MiniFooter";
+import ResponsiveFooter from "../Footer/ResponsiveFooter"; // Правильный путь к компоненту ResponsiveFooter
 import styles from "./layout.module.css";
 
 interface LayoutProps {
@@ -12,24 +11,20 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   const location = useLocation(); // Получение текущего маршрута
 
-  // Список страниц, где нужно отображать мини-футер
-  const miniFooterPages = ["/contacts", "/another-page"]; 
-  const isMiniFooter = miniFooterPages.includes(location.pathname); // Проверка, нужен ли мини-футер
+  // Страницы без футера вообще
+  const noFooterPages = ["/admin-panel", "/admin-panel/doctors", "/panel/appointments", "/admin-panel/services"];
+  const hideFooter = noFooterPages.some((path) => location.pathname.startsWith(path)); 
 
-   // Страницы без футера вообще
-   const noFooterPages = ["/admin-panel", "/admin-panel/doctors", "/panel/appointments", "/admin-panel/services"];
-   const hideFooter = noFooterPages.some((path) => location.pathname.startsWith(path)); 
-
-   return (
+  return (
     <div className={styles.layout}>
       {/* Шапка сайта */}
       <Header />
       
       {/* Основное содержимое */}
       <main className={styles.content}>{children}</main>
-
+      
       {/* Условное отображение футера */}
-      {!hideFooter && (isMiniFooter ? <MiniFooter /> : <Footer />)}
+      {!hideFooter && <ResponsiveFooter />}
     </div>
   );
 };
