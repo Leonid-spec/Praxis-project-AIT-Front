9d26c29e0f9344fa2f77b4b205c07ae9bb0e39c8
Tanya Maxar <kolt.levry@gmail.com>
2025-04-15 09:55:16 +0200

    add Yuliias setting translation

--- src/pages/AdminPanel/Settings/RunningLine/RunningLinePage.tsx
+++ src/pages/AdminPanel/Settings/RunningLine/RunningLinePage.tsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
-import "./RunningLinePage.css";
+import { useTranslation } from "react-i18next";
 import Notification from "../../../../components/Notification/Notification";
+import "./RunningLinePage.css";
 
 const RunningLinePage: React.FC = () => {
+  const { t } = useTranslation();
   const [runningText, setRunningText] = useState("");
   const [notification, setNotification] = useState<{
     message: string;
     type: "error" | "success";
   } | null>(null);
 
-  // Загружаем текст из localStorage при первом рендере
   useEffect(() => {
     const savedText = localStorage.getItem("runningLineText");
     if (savedText) {
@@ -21,13 +22,13 @@ const RunningLinePage: React.FC = () => {
     try {
       localStorage.setItem("runningLineText", runningText);
       setNotification({
-        message: "Бегущая строка успешно сохранена!",
+        message: t("message.adminPanel.appointments.settings.admin.settingsPage.runningLine.successMessage"),
         type: "success",
       });
     } catch (error) {
       console.error("Ошибка при сохранении в localStorage:", error);
       setNotification({
-        message: "Произошла ошибка при сохранении. Попробуйте снова.",
+        message: t("message.adminPanel.appointments.settings.admin.settingsPage.runningLine.errorMessage"),
         type: "error",
       });
     }
@@ -35,16 +36,16 @@ const RunningLinePage: React.FC = () => {
 
   return (
     <div className="running-line-page-container">
-      <h2 className="title">Настройка бегущей строки</h2>
+      <h2 className="title">{t("message.adminPanel.appointments.settings.admin.settingsPage.runningLine.title")}</h2>
       <textarea
         className="text-area"
         value={runningText}
         onChange={(e) => setRunningText(e.target.value)}
-        placeholder="Введите текст для бегущей строки..."
+        placeholder={t("message.adminPanel.appointments.settings.admin.settingsPage.runningLine.placeholder")}
       />
       <div className="save-button-container">
         <button className="save-button" onClick={handleSave}>
-          Сохранить
+          {t("message.adminPanel.appointments.settings.admin.settingsPage.runningLine.saveButton")}
         </button>
       </div>
       {notification && (
