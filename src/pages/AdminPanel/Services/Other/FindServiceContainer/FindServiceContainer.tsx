9da8b41c37683a442da6f34e4df18fddced1e783
Tanya Maxar <kolt.levry@gmail.com>
2025-04-24 17:43:20 +0200

    add global styles to all btns

--- src/pages/AdminPanel/Services/Other/FindServiceContainer/FindServiceContainer.tsx
+++ src/pages/AdminPanel/Services/Other/FindServiceContainer/FindServiceContainer.tsx
@@ -1,10 +1,6 @@
-import { 
-  InputBox, 
-  SearchIcon, 
-  StyledButton 
-} from "./styles";
 import { useTranslation } from "react-i18next";  
 import { FaSearch } from "react-icons/fa";
+import { InputBox, SearchIcon, StyledButton } from "../../../AppointmentsPage/MainPage/FindByTime/styles";
 
 interface FindServiceContainerProps {
   searchTerm: string;
@@ -22,7 +18,7 @@ export const FindServiceContainer: React.FC<FindServiceContainerProps> = ({ sear
   return (
     <StyledButton>
       <SearchIcon>
-        <FaSearch style={{ color: "white", fontSize: "24px" }} />
+        <FaSearch style={{ color: "white" }} />
       </SearchIcon>
       <InputBox 
         type="text" 
