5796ea312a8df8a5b8a76313e9859a17d1448478
Tanya Maxar <kolt.levry@gmail.com>
2025-04-25 19:46:10 +0200

    fixed bug

--- src/pages/AdminPanel/AdminPanelPage/AdminPanel.tsx
+++ src/pages/AdminPanel/AdminPanelPage/AdminPanel.tsx
@@ -1,29 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import styles from "./adminPanel.module.css";
 import Sidebar from "./Sidebar";
 import MainContent from "./MainContent";
 
-const AdminPanel: React.FC = () => {
-  const [hasReloaded, setHasReloaded] = useState(false);
-  const navigate = useNavigate();
-  const token = localStorage.getItem("token");
+interface AdminPanelProps {
+  isLoggedIn?: boolean;
+}
 
-  useEffect(() => {
-    if (!token) {
-      navigate("/"); 
-    } else if (!hasReloaded) {
-      setHasReloaded(true);
-      const alreadyReloaded = sessionStorage.getItem("alreadyReloaded");
-      if (!alreadyReloaded) {
-        sessionStorage.setItem("alreadyReloaded", "true");
-        window.location.reload(); 
-      }
-    }
-  }, [token, hasReloaded, navigate]);
-
-  if (!token) {
-    return null; 
+const AdminPanel: React.FC<AdminPanelProps> = ({ isLoggedIn }) => {
+  if (!isLoggedIn) {
+  return <div>Please log in</div>;
   }
 
   return (
