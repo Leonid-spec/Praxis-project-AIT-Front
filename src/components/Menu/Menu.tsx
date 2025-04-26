b24df04ba455b08a0a0187e346c3f58900bdd043
Tanya Maxar <kolt.levry@gmail.com>
2025-04-26 21:14:51 +0200

    st

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,102 +1,72 @@
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import AdminMenu from "../AdminMenu/AdminMenu";
-import {
-  MenuContainer,
-  Logo,
-  Nav,
-  StyledNavLink,
-  SprachUundAdminbereich,
-  LanguagePanel,
-  LanguageLink,
-  Divider,
-  NavLinkImg,
-  NavLinkText,
-  BesideLogoContainer,
-} from "./styles";
-import BurgerMenu from "./BurgerMenu";
-import React from "react";
 import MakeAppointmentBtn from "../Button/MakeAppointmentBtn/MakeAppointmentBtn";
+import BurgerMenu from "./BurgerMenu";
 import LanguageIcon from "./LanguageIcon";
+import { LanguageLink, Divider } from "./BurgerMenuStyles";
+import { MenuContainer, LogoContainer, StyledNavLink, LogoImage, LogoText, RightContainer, Navigation, NavItem, LanguageAndAdmin, LanguageSwitcher } from "./styles";
 
-const Menu = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(() => {
-    return localStorage.getItem("token") !== null;
-  });
-  
+const Menu: React.FC = () => {
   const { t, i18n } = useTranslation();
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth < 1280);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(!!localStorage.getItem("token"));
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 1280);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handleLanguageChange = (lng: string) => {
     i18n.changeLanguage(lng);
   };
 
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 1280);
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
     <MenuContainer>
-      <Logo>
+      <LogoContainer>
         <StyledNavLink to="/" aria-label={t("message.menu.home")}>
-          <NavLinkImg src="/images/MainLogo.jpg" alt="Logo" />
+          <LogoImage src="/images/MainLogo.jpg" alt="Logo" />
         </StyledNavLink>
         <StyledNavLink to="/" aria-label={t("message.menu.home")}>
-          <NavLinkText>Zahnarztpraxis Sofia Abramian</NavLinkText>
+          <LogoText>Zahnarztpraxis Sofia Abramian</LogoText>
         </StyledNavLink>
-      </Logo>
+      </LogoContainer>
 
-      <BesideLogoContainer>
-        {!isMobile ? (
+      <RightContainer>
+        {isMobile ? (
           <>
-            <Nav>
-              <StyledNavLink to="/services">
-                {t("message.header.menu.services")}
-              </StyledNavLink>
-              <StyledNavLink to="/about">
-                {t("message.header.menu.about_us")}
-              </StyledNavLink>
-              <StyledNavLink to="/team">
-                {t("message.header.menu.team")}
-              </StyledNavLink>
-              <StyledNavLink to="/contacts">
-                {t("message.header.menu.contact")}
-              </StyledNavLink>
-            </Nav>
+            <LanguageIcon />
+            <BurgerMenu />
+          </>
+        ) : (
+          <>
+            <Navigation>
+              <NavItem to="/services">{t("message.header.menu.services")}</NavItem>
+              <NavItem to="/about">{t("message.header.menu.about_us")}</NavItem>
+              <NavItem to="/team">{t("message.header.menu.team")}</NavItem>
+              <NavItem to="/contacts">{t("message.header.menu.contact")}</NavItem>
+            </Navigation>
+
             <MakeAppointmentBtn text={t("message.main.use_oft.button.title")} />
 
-            <SprachUundAdminbereich>
-              <LanguagePanel>
+            <LanguageAndAdmin>
+              <LanguageSwitcher>
                 {["de", "en", "ru"].map((lang, index) => (
                   <React.Fragment key={lang}>
-                    <LanguageLink
-                      onClick={(e) => {
-                        e.preventDefault();
-                        handleLanguageChange(lang);
-                      }}
-                    >
+                    <LanguageLink onClick={() => handleLanguageChange(lang)}>
                       {lang.toUpperCase()}
                     </LanguageLink>
                     {index < 2 && <Divider>|</Divider>}
                   </React.Fragment>
                 ))}
-              </LanguagePanel>
-              <AdminMenu
-                isLoggedIn={isLoggedIn}
-                setIsLoggedIn={setIsLoggedIn}
-              />
-            </SprachUundAdminbereich>
-          </>
-        ) : (
-          <>
-            <LanguageIcon />
-            <BurgerMenu />
+              </LanguageSwitcher>
+
+              <AdminMenu isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+            </LanguageAndAdmin>
           </>
         )}
-      </BesideLogoContainer>
+      </RightContainer>
     </MenuContainer>
   );
 };
